5d20aec5152f32cd66f3a51c041c3f0b7e494317
Aleksey Kuznetsov <thealekzzz@yandex.ru>
2021-08-21 18:33:53 +0300

    working with pictures

--- form.php
+++ form.php
@@ -1,5 +1,5 @@
 <?php
-var_dump($_FILES);
+// var_dump($_FILES);
 echo "<br>";
 $inputName = "photo";
 
@@ -17,17 +17,31 @@ function print_files() {
 
 if (move_uploaded_file($_FILES[$inputName]["tmp_name"], 'images/photos/'.$_FILES[$inputName]["name"])) {
     $photoPath = 'images/photos/'.$_FILES[$inputName]["name"];
-    echo "Файл скопирован в " . $photoPath;
+    // echo "Файл скопирован в " . $photoPath;
 } else {
     echo "Файл не скопирован";
 }
 
 if (move_uploaded_file($_FILES["file"]["tmp_name"], 'images/photos/'.$_FILES["file"]["name"])) {
     $photoPath2 = 'images/photos/'.$_FILES["file"]["name"];
-    echo "Файл 2 скопирован в " . $photoPath2;
+    // echo "Файл 2 скопирован в " . $photoPath2;
 } else {
     echo "Файл 2 не скопирован";
 }
 
-$image = imagecreatefromjpeg($photoPath);
+// header('Content-Type: image/jpeg');
 
+$image = imagecreatefromjpeg($photoPath);
+$src_image = imagecreatefromjpeg($photoPath2);
+
+$image_dim = getimagesize($photoPath);
+$src_dim = getimagesize($photoPath2);
+
+imagecopy($image, $src_image, 50, 50, 0, 0, $src_dim[0], $src_dim[1]);
+
+imagejpeg($image, 'res.jpg');
+
+imagedestroy($image);
+imagedestroy($src_image);
+
+echo "<img src='res.jpg'>";   
